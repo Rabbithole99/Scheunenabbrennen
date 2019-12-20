dccf831360e0c3846051f5da4a1ba8abb93665d7
Rabbithole99 <54251240+Rabbithole99@users.noreply.github.com>
2019-12-20 17:22:01 +0100

    Overhaul

---
+++ ScheuneVR/Assets/VR Game/scripts/GazeTeleport.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class GazeTeleport : MonoBehaviour
+{
+
+
+    public Image imgGaze;
+
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+            if(imgGaze.fillAmount == 1 && _hit.transform.CompareTag("Teleport"))
+            {
+
+                _hit.transform.gameObject.GetComponent<Teleport>().TeleportPlayer();
+            }
+        }
+
+    }
+
+
+    public void GVROn()
+    {
+
+        gvrStatus = true;
+
+    }
+
+    public void GVROFF()
+    {
+
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+
+
+}
