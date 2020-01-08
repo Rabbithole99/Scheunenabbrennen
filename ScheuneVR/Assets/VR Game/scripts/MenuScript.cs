3b679991157b559fd442839560aae4ba016dad98
Rabbithole99 <54251240+Rabbithole99@users.noreply.github.com>
2020-01-08 23:31:06 +0100

    Menu_Implementation_GazeContrl

---
+++ ScheuneVR/Assets/VR Game/scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public void SceneSwitch(){
+        SceneManager.LoadScene(1);}
+
+    public void ExitAPP()
+    {
+        Application.Quit();
+    }
+
+   
+}
