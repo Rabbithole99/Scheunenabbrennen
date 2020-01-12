4c65b4f8b00bf6499deb5d4c41e05580e8bae452
Rabbithole99 <54251240+Rabbithole99@users.noreply.github.com>
2020-01-12 22:06:03 +0100

    ImplementedIngameMenu
    menu selection by gazing on ceiling

---
+++ ScheuneVR/Assets/VR Game/scripts/IngameOpenMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class IngameOpenMenu : MonoBehaviour
+{
+
+
+    public Image imgGaze;
+
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+
+
+
+   
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+            if (imgGaze.fillAmount == 1 && _hit.transform.CompareTag("IngameMenu"))
+            {
+
+                SceneManager.LoadScene(0); 
+            }
+        }
+
+    }
+
+
+    public void GVROn()
+    {
+
+        gvrStatus = true;
+
+    }
+
+    public void GVROFF()
+    {
+
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+
+
+}
