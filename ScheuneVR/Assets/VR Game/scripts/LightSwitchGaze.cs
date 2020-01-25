a93294f3822e6348cc2ea2fe5384b49a2f90a9eb
Rabbithole99 <54251240+Rabbithole99@users.noreply.github.com>
2020-01-25 21:36:39 +0100

    Prototype

---
+++ ScheuneVR/Assets/VR Game/scripts/LightSwitchGaze.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class LightSwitchGaze : MonoBehaviour
+{
+
+    public Image ImgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrstatus;
+    public float gvrTimer;
+    public Scene first;
+
+
+
+    void Update()
+    {
+        if (gvrstatus)
+        {
+            gvrTimer += Time.deltaTime;
+            ImgCircle.fillAmount = gvrTimer / totalTime;
+        }
+        if (gvrTimer > totalTime)
+        {
+            this.GetComponent<Light>().enabled = false;
+
+
+        }
+    }
+
+    public void GvrOn()
+    {
+        gvrstatus = true;
+    }
+
+    public void GvrOff()
+    {
+        gvrstatus = false;
+        gvrTimer = 0;
+        ImgCircle.fillAmount = 0;
+    }
+
+
+}
