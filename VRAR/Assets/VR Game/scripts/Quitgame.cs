ca96425fe323bec896454b17546627f8073d8b93
Rabbithole99 <54251240+Rabbithole99@users.noreply.github.com>
2020-09-04 22:33:21 +0200

    VRAR
    copy

---
+++ VRAR/Assets/VR Game/scripts/Quitgame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quitgame : MonoBehaviour
+{
+ public GameObject completeLevelUI;
+	 
+	 public void CompleteLevel () 
+	 {
+		 completeLevelUI.SetActive(true);
+         SceneManager.LoadScene(1);
+	 }
+	 
+	 void OnTriggerEnter ()
+	 {
+		 CompleteLevel ();
+         
+        
+    }
+}
