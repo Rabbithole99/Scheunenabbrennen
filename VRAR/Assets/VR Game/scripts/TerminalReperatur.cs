8690e7c29cdc825ca53dac3be59104c9bd00b505
EstebanCazador <sjaeger6@smail.uni-koeln.de>
2020-09-07 14:43:39 +0200

    TerminalReperatur&Taskreihenfolge

---
+++ VRAR/Assets/VR Game/scripts/TerminalReperatur.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalReperatur : MonoBehaviour
+{
+
+   public GameObject dings;
+    // Start is called before the first frame update
+   public void  reparieren()
+    {
+        dings.SetActive(false);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
